7d715e9fb0e2e854205a513cdd07eac51d730261
neul <mgh.neul@gmail.com>
2025-07-28 21:07:43 +0900

    add employee api

---
+++ src/main/java/com/fastcampus/projectvoucher/app/request/EmployeeCreateRequest.java
@@ -0,0 +1,4 @@
+package com.fastcampus.projectvoucher.app.request;
+
+public record EmployeeCreateRequest(String name, String position, String department) {
+}
---
+++ src/main/java/com/fastcampus/projectvoucher/app/response/EmployeeResponse.java
@@ -0,0 +1,4 @@
+package com.fastcampus.projectvoucher.app.response;
+
+public record EmployeeResponse(Long no, String name, String position, String department) {
+}
