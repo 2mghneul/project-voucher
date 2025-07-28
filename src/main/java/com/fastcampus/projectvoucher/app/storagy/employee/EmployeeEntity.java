249debdb8ad47675f7377b707aa3b33127ed2aff
neul <mgh.neul@gmail.com>
2025-07-28 22:06:38 +0900

    영속성 저장소를 service 레이어로 이동

---
+++ src/main/java/com/fastcampus/projectvoucher/app/storagy/employee/EmployeeEntity.java
@@ -0,0 +1,45 @@
+package com.fastcampus.projectvoucher.app.storagy.employee;
+
+import jakarta.persistence.*;
+
+@Table(name = "employee")
+@Entity
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String position;
+
+    @Column
+    String department;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(String name, String position, String department) {
+        this.name = name;
+        this.position = position;
+        this.department = department;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String position() {
+        return position;
+    }
+
+    public String department() {
+        return department;
+    }
+}
