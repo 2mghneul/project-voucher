d7aba9e84029e1fa7794fe6562b15ae7418a5e30
neul <mgh.neul@gmail.com>
2025-07-28 23:15:08 +0900

    add swagger http://localhost:8080/swagger-ui/index.html 에 접속하여 정리된 api 문서 확인 가능

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 }
 
 tasks.withType<Test> {
