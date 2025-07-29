bb6a53b54030a8fae9835f3d5bfd25ad419bacde
neul <mgh.neul@gmail.com>
2025-07-29 14:49:30 +0900

    표준 예외 처리 리팩토링 BiFunction 사용

--- src/main/java/com/fastcampus/projectvoucher/app/common/exception/ApiControllerAdvice.java
+++ src/main/java/com/fastcampus/projectvoucher/app/common/exception/ApiControllerAdvice.java
@@ -2,15 +2,15 @@ package com.fastcampus.projectvoucher.app.common.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Arrays;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
 @RestControllerAdvice
 public class ApiControllerAdvice {
@@ -19,21 +19,37 @@ public class ApiControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException e) {
-        logger.info(Arrays.toString(e.getStackTrace()));
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now(), UUID.randomUUID());
+//        logger.info(Arrays.toString(e.getStackTrace()));
+//        return createErrorResponse(e.getMessage());
+        return createErrorResponse.apply(e, Level.INFO);
     }
+
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalStateException.class)
     public ErrorResponse handleIllegalStateException(final IllegalStateException e) {
-        logger.info(Arrays.toString(e.getStackTrace()));
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now(), UUID.randomUUID());
+        return createErrorResponse.apply(e, Level.INFO);
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ErrorResponse handleException(final Exception e) {
-        logger.error(Arrays.toString(e.getStackTrace()));
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now(), UUID.randomUUID());
+        return createErrorResponse.apply(e, Level.ERROR);
     }
+
+//    private static ErrorResponse createErrorResponse(String message) {
+//        return new ErrorResponse(message, LocalDateTime.now(), UUID.randomUUID());
+//    }
+
+    private final BiFunction<Exception, Level, ErrorResponse> createErrorResponse = (e, level) -> {
+        final ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), LocalDateTime.now(), UUID.randomUUID());
+
+        if (level == Level.ERROR) {
+            logger.error("traceId: {}", errorResponse.traceId(), e);
+        } else if (level == Level.INFO) {
+            logger.info("traceId: {}", errorResponse.traceId(), e);
+        }
+
+        return errorResponse;
+    };
 }
