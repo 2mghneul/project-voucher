59b614d0b1055605ffea70fe3e6363acb119e61f
neul <mgh.neul@gmail.com>
2025-07-29 21:48:44 +0900

    검증 로직 리팩토링 검증에 한하여 메소드 이름을 한글로 만들어 보자

--- src/main/java/com/fastcampus/projectvoucher/app/domain/voucher/validator/VoucherDisableValidator.java
+++ src/main/java/com/fastcampus/projectvoucher/app/domain/voucher/validator/VoucherDisableValidator.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class VoucherDisableValidator {
     public void validate(VoucherEntity voucherEntity, RequestContext requestContext) {
+        발품권_사용_불가_권한이_있는지_확인(voucherEntity, requestContext);
+    }
+
+    private static void 발품권_사용_불가_권한이_있는지_확인(VoucherEntity voucherEntity, RequestContext requestContext) {
         if (voucherEntity.publishHistory().requesterType() != requestContext.requesterType() || !voucherEntity.publishHistory().requesterId().equals(requestContext.requesterId())) {
             throw  new IllegalArgumentException("사용 불가 처리 권한이 없는 상품권 입니다.");
         }
